9b2568faae97bd9f986c119291fae6b6d25dbd9b
EncryptedHacker <77277835+PanchananNath@users.noreply.github.com>
2021-06-06 22:10:37 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Interest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double amount, interest, time;
+
+
+
+            double simple_interest;
+
+
+
+
+
+            amount = Convert.ToDouble(textBox1.Text);
+
+
+
+            interest = Convert.ToDouble(textBox2.Text);
+
+
+
+            time = Convert.ToDouble(textBox2.Text);
+
+
+
+            simple_interest = amount * interest * time / 100;
+
+
+
+            textBox4.Text = simple_interest.ToString("0.00");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            textBox2.Text = "";
+
+            textBox3.Text = "";
+
+            textBox4.Text = "";
+
+            textBox1.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
