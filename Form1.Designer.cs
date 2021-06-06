8d4452ab089ffbf3e14e54ee1b7b7737cc71ef30
EncryptedHacker <77277835+PanchananNath@users.noreply.github.com>
2021-06-06 22:13:26 +0530

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -3,15 +3,16 @@ namespace Simple_Interest
 {
     partial class Form1
     {
-        /// <summary>
+        
         /// Required designer variable.
-        /// </summary>
+        
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+     
         /// Clean up any resources being used.
-        /// </summary>
+       
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -23,10 +24,10 @@ namespace Simple_Interest
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
+      
+     
         /// the contents of this method with the code editor.
-        /// </summary>
+        
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
